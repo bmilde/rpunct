266a2591c7f6d19f4fa25d5919f6c44a297b8837
Alienmaster <robert.geislinger@uni-hamburg.de>
2022-05-27 13:17:28 +0200

    made serval speed changes to prep_data

--- training/prep_data.py
+++ training/prep_data.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # 💾⚙️🔮
 
-__author__ = "Daulet N."
-__email__ = "daulet.nurmanbetov@gmail.com"
+__author__ = "Daulet N., Robert Geislinger"
+__email__ = "daulet.nurmanbetov@gmail.com, github@crpykng.de"
 
 import os
 import re
@@ -10,18 +10,20 @@ import json
 import random
 import pandas as pd
 
-
 def create_train_datasets():
     output_file_names = []
-    download_df()
-    for i in ['yelp_polarity_reviews_train.csv', 'yelp_polarity_reviews_test.csv']:
+    # download_df()
+    print('start processing')
+    for i in 'abcdefghijk':
+        i = 'subs_norm1_puncta' + i
+        print(f'{i=}')
         name = i.split(".")[0]
         split_nm = name.split("_")[-1]
         df_name = name.split("_")[0]
-        create_rpunct_dataset(i, f"{name}_data.json")
+        all_records = create_rpunct_dataset(i)
         output_file_names.append(f"{df_name}_{split_nm}.txt")
-        create_training_samples(f"{name}_data.json", f"{df_name}_{split_nm}.txt")
-        return output_file_names
+        create_training_samples(all_records, f"{df_name}_{split_nm}")
+    return output_file_names
 
 
 def download_df(dir_path=''):
@@ -30,7 +32,8 @@ def download_df(dir_path=''):
     ds = tfds.load('yelp_polarity_reviews', split=data_type, shuffle_files=True)
     for i in ds:
         i = tfds.as_dataframe(i)
-        csv_path = os.path.join(dir_path, f'yelp_polarity_reviews_{i}.csv')
+        print(i['label'][0])
+        csv_path = os.path.join(dir_path, f'yelp_polarity_reviews_{i["label"][0]}.csv')
         i.to_csv(csv_path, index=False)
 
 
@@ -40,8 +43,7 @@ def create_record(row):
     """
     pattern = re.compile("[\W_]+")
     new_obs = []
-
-    observation = eval(row).decode().replace('\\n', ' ').split()
+    observation = row.replace('\\n', ' ').split()
 
     for obs in observation:
         text_obs = obs.lower()
@@ -62,49 +64,74 @@ def create_record(row):
     return new_obs
 
 
-def create_rpunct_dataset(orig_yelp_dataframe, rpunct_dataset_path='rpunct_data.json'):
-    df = pd.read_csv(orig_yelp_dataframe)
-    # Filter to only positive examples
-    df = df[df['label'] == 1].reset_index(drop=True)
-    # Dataframe Shape
+def threadingPart(df):
+    print('start threading')
+    _all_records = []
+    for i in df[0]:
+        records = create_record(i)
+        _all_records.extend(records)
+    print('end threading')
+    return _all_records
+
+def create_rpunct_dataset(orig_data):
+    print('create dataset')
+    df = pd.DataFrame()
+    input = ''
+    with open(orig_data) as f:
+        input=f.read()
+    inputA = input.split('\n')
+    df = pd.DataFrame(inputA)
+    # chunk_size = int(df.shape[0]/prs)
+    # chunks = [df.iloc[df.index[i:i+chunk_size]] for i in range(0, df.shape[0], chunk_size)]
+    all_records = []
+    for i in df[0]:
+        records = create_record(i)
+        all_records.extend(records)
+    
+    # with Pool(prs) as p:
+    #     all_records = p.map(threadingPart, chunks)
+
     print(f"Dataframe samples: {df.shape}")
 
-    all_records = []
-    for i in range(df.shape[0]):
-        orig_row = df['text'][i]
-        records = create_record(orig_row)
-        all_records.extend(records)
-
-    with open(rpunct_dataset_path, 'w') as fp:
-        json.dump(all_records, fp)
+    return all_records
+    # with open(rpunct_dataset_path, 'w') as fp:
+    #     json.dump(all_records, fp)
 
 
-def create_training_samples(json_loc_file, file_out_nm='train_data', num_splits=5):
+def create_training_samples(all_records, file_out_nm='train_data', num_splits=5):
     """
     Given a looong list of tokens, splits them into 500 token chunks
     thus creating observations. This is for fine-tuning with simpletransformers
     later on.
     """
+    print('create training samples')
     random.seed(1337)
-    observations = []
+    
     _round = 0
-
+    # all_recs = all_records
     while _round < num_splits:
-        with open(json_loc_file, 'r') as fp:
-            all_records = json.load(fp)
+        all_recs = all_records
+        observations = []
+        # with open(json_loc_file, 'r') as fp:
+        #     all_records = json.load(fp)
 
-        size = len(all_records) // num_splits
-        all_records = all_records[size * _round:size * (_round + 1)]
-        splits = create_tokenized_obs(all_records)
-        full_data = pd.DataFrame(all_records)
-        del all_records
+        size = len(all_recs) // num_splits
+        all_recs = all_recs[size * _round:size * (_round + 1)]
+        splits = create_tokenized_obs(all_recs)
+        full_data = pd.DataFrame(all_recs)
+        del all_recs
 
         for i in splits:
             data_slice = full_data.iloc[i[0]:i[1], ]
-            observations.append(data_slice.values.tolist())
+            obi = data_slice.values.tolist()
+            obi2 = []
+            for a in obi:
+                if a:
+                    obi2.append(a)
+
+            observations.append(obi2)
         _round += 1
         random.shuffle(observations)
-
         with open(f'{file_out_nm}_{_round}.txt', 'w') as fp2:
             json.dump(observations, fp2)
 
@@ -119,10 +146,12 @@ def create_tokenized_obs(input_list, num_toks=500, offset=250):
     It is important that first token is capitalized and we fed as many tokens as possible.
     In a real use-case we will not know where splits are so we'll just feed all tokens till limit.
     """
+    print('create tokenized observations')
     start = -1
     loop_end = -1
     appends = []
     for ix, i in enumerate(input_list):
+        # print(f'{ix:}')
         if ix == loop_end:
             start = -1
         if i['labels'][-1] == "U" and start == -1:
